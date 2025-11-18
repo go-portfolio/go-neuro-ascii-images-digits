4f9a85f85be0a0b1d99a47c2fe1cb1c02b52b91f
aleksey <av.pavlow@gmail.com>
2025-11-18 12:39:17 +0300

    feat: Сделать изображения более чёткими - Изображения сделаны более чёткими

--- internal/data/visual.go
+++ internal/data/visual.go
@@ -11,15 +11,16 @@ func PrintFeatureMap(map2D [][]float64, title string) {
 		for x := 0; x < len(map2D[0]); x++ {
 			v := map2D[y][x]
 			switch {
-			case v > 0.7:
+			case v > 0.2:
 				fmt.Print("#")
-			case v > 0.4:
-				fmt.Print("*")
 			case v > 0.1:
+				fmt.Print("*")
+			case v > 0.01:
 				fmt.Print("+")
 			default:
 				fmt.Print(".")
 			}
+
 		}
 		fmt.Println()
 	}
